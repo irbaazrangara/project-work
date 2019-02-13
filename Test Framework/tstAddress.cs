f9bd139076e00021df865d86730e5dcfb428a80a
p17199779 <p17199779@my365.dmu.ac.uk>
2019-02-13 12:11:16 +0000

    creation of clsAddress+test class

---
+++ Test Framework/tstAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstAddress
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create an instance of our class clsAddress
+            clsAddress AnAddress = new clsAddress();
+            //check to see what the class is not null
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
